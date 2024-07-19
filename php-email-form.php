c0b6e117c0a3d8f00b97a23695b54ea064a652c4
Yaksh Darji <95200752+dyaksh@users.noreply.github.com>
2024-07-19 21:54:27 +0530

    updated files

--- php-email-form.php
+++ php-email-form.php
@@ -1,7 +1,7 @@
 <?php
 
 class PHP_Email_Form {
-  public $to = '';
+  public $to = 'yakshdarji2@gmai.com';
   public $from_name = '';
   public $from_email = '';
   public $subject = '';
--- validate.js
+++ validate.js
@@ -1,33 +1,19 @@
-/**
-* PHP Email Form Validation - v3.8
-* URL: https://bootstrapmade.com/php-email-form/
-* Author: BootstrapMade.com
-*/
+
 (function () {
   "use strict";
 
   let forms = document.querySelectorAll('.php-email-form');
 
-<<<<<<< HEAD
-  forms.forEach(function (e) {
-    e.addEventListener('submit', function (event) {
-=======
   forms.forEach( function(e) {
     e.addEventListener('submit', function(event) {
->>>>>>> e29c4aa35e2731b2a3fdd1a14bf507441a9deaf2
       event.preventDefault();
 
       let thisForm = this;
 
       let action = thisForm.getAttribute('action');
       let recaptcha = thisForm.getAttribute('data-recaptcha-site-key');
-<<<<<<< HEAD
 
       if (!action) {
-=======
-      
-      if( ! action ) {
->>>>>>> e29c4aa35e2731b2a3fdd1a14bf507441a9deaf2
         displayError(thisForm, 'The form action property is not set!');
         return;
       }
@@ -35,7 +21,6 @@
       thisForm.querySelector('.error-message').classList.remove('d-block');
       thisForm.querySelector('.sent-message').classList.remove('d-block');
 
-<<<<<<< HEAD
       let formData = new FormData(thisForm);
 
       if (recaptcha) {
@@ -48,20 +33,6 @@
                   php_email_form_submit(thisForm, action, formData);
                 })
             } catch (error) {
-=======
-      let formData = new FormData( thisForm );
-
-      if ( recaptcha ) {
-        if(typeof grecaptcha !== "undefined" ) {
-          grecaptcha.ready(function() {
-            try {
-              grecaptcha.execute(recaptcha, {action: 'php_email_form_submit'})
-              .then(token => {
-                formData.set('recaptcha-response', token);
-                php_email_form_submit(thisForm, action, formData);
-              })
-            } catch(error) {
->>>>>>> e29c4aa35e2731b2a3fdd1a14bf507441a9deaf2
               displayError(thisForm, error);
             }
           });
@@ -78,7 +49,6 @@
     fetch(action, {
       method: 'POST',
       body: formData,
-<<<<<<< HEAD
       headers: { 'X-Requested-With': 'XMLHttpRequest' }
     })
       .then(response => {
@@ -100,29 +70,6 @@
       .catch((error) => {
         displayError(thisForm, error);
       });
-=======
-      headers: {'X-Requested-With': 'XMLHttpRequest'}
-    })
-    .then(response => {
-      if( response.ok ) {
-        return response.text();
-      } else {
-        throw new Error(`${response.status} ${response.statusText} ${response.url}`); 
-      }
-    })
-    .then(data => {
-      thisForm.querySelector('.loading').classList.remove('d-block');
-      if (data.trim() == 'OK') {
-        thisForm.querySelector('.sent-message').classList.add('d-block');
-        thisForm.reset(); 
-      } else {
-        throw new Error(data ? data : 'Form submission failed and no error message returned from: ' + action); 
-      }
-    })
-    .catch((error) => {
-      displayError(thisForm, error);
-    });
->>>>>>> e29c4aa35e2731b2a3fdd1a14bf507441a9deaf2
   }
 
   function displayError(thisForm, error) {
